2faac2ca9c5b1672e34c2e4835e69ecb197d2566
Sarimoko <TravisReames@gmail.com>
2016-10-03 08:55:51 -0700

    Update kotk.php
    test

--- api/kotk.php
+++ api/kotk.php
@@ -1,1 +1,20 @@
-init
+<?php
+
+$db = simplexml_load_file("db_kotk.xml");
+$category = $db->Category;
+
+foreach($category->Name as $name)
+{
+    $menu = (string)$name;
+    $i = 0;
+    if ($menu == "CAT1" )
+{
+        echo $category->Location->Room[$i];
+        $i++;       
+}
+else 
+{
+        echo "No result";
+}
+}
+?>
